bec862016899eb57ffd8dc1eaf341e113fae265f
Raythx98 <65889977+raythx98@users.noreply.github.com>
2021-05-18 21:26:28 +0800

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -1,8 +1,7 @@
-//  Work experience cards
+//  Technical experience cards
 
-
-const experiencecards = document.querySelector(".experience-cards");
-const exp = [
+const techexperiencecards = document.querySelector(".technical-experience-cards");
+const techexp = [
   {
     title: "Software Development Intern",
     cardImage: "assets/images/experience-page/flipkart.jpg",
@@ -26,7 +25,7 @@ const exp = [
   },
 ];
 
-const showCards2 = () => {
+const techShowCards2 = () => {
   let output = "";
   exp.forEach(
     ({ title, cardImage, place, time, desp }) =>
@@ -54,7 +53,65 @@ const showCards2 = () => {
   );
   experiencecards.innerHTML = output;
 };
-document.addEventListener("DOMContentLoaded", showCards2);
+document.addEventListener("DOMContentLoaded", techShowCards2);
+
+//  Work experience cards
+
+
+const workexperiencecards = document.querySelector(".technical-experience-cards");
+const workexp = [
+  {
+    title: "Software Development Intern",
+    cardImage: "assets/images/experience-page/flipkart.jpg",
+    place: "Flipkart",
+    time: "(May, 2020 - present)",
+    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+  },
+  {
+    title: "Student Developer",
+    cardImage: "assets/images/experience-page/gsoc.png",
+    place: "Google Summer Of Code",
+    time: "(Mar - Aug, 2019)",
+    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+  },
+  {
+    title: "Research Intern",
+    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
+    place: "IIT, Bombay",
+    time: "(Dec, 19 - Jan, 20)",
+    desp:"<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+  },
+];
+
+const workShowCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, place, time, desp }) =>
+      (output += `        
+      <ul>
+      <li class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="pre-heading">${place}</span><br>
+              <span class="author">${time}</span>
+            </p>
+            <ol>
+              ${desp}
+            </ol>
+          </header>
+        </article>
+      </li>
+    </ul>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", workShowCards2);
 
 
 // Volunteership Cards
