8b4a1b8fe9ea57eff7bd0ff688741f3a9ba96681
Raythx98 <65889977+raythx98@users.noreply.github.com>
2021-05-20 16:42:40 +0800

    Update research.js

--- assets/js/research.js
+++ assets/js/research.js
@@ -41,27 +41,21 @@ const fillData = () => {
                         <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${absbox}" aria-expanded="false" aria-controls="${absbox}">
                             ABSTRACT
                         </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                
-                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${citebox}" aria-expanded="false" aria-controls="${citebox}">
-                            CITATION
-                        </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     </div>
-                    <div class="d-flex" style="margin-right:5%;">
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            
-                    <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${citebox}" aria-expanded="false" aria-controls="${citebox}">
-                        CITATION
-                    </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                </div>
                     <div id="${absbox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
                         <div class="card-body">
                             ${abstract}    
                         </div>
                     </div>
+                    <div class="d-flex" style="margin-right:5%;">
+            
+                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${citebox}" aria-expanded="false" aria-controls="${citebox}">
+                            CITATION
+                        </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    </div>
                     <div id="${citebox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
                         <div class="card-body">
                             ${citation.vancouver}    
-                        </div>
                     </div>
                 </td>
             </tr>`)
