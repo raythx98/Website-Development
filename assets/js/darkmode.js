d7cb18a98f5524ba34e246715953c254d86cfe3d
Raythx98 <65889977+raythx98@users.noreply.github.com>
2021-05-21 00:44:47 +0800

    Attempt dark mode

---
+++ assets/js/darkmode.js
@@ -0,0 +1,7 @@
+let darkMode = localStorage.getItem('darkMode');
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+darkModeToggle.addEventListener('click', () => {
+    console.log('test')''
+})
