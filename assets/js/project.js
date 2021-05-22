895ed82c6d9662d88864d6efd2e4f306dc59badf
Raythx98 <65889977+raythx98@users.noreply.github.com>
2021-05-22 23:50:16 +0800

    Clamp

--- assets/js/project.js
+++ assets/js/project.js
@@ -95,7 +95,7 @@ const showCards = () => {
             </div>
           </div>
           <div class="subcontent">
-              <a class="subtitle">${description}</a>
+              <a class="subtitle clamp8">${description}</a>
           </div>
         <ul class="menu-content"><br>
               <li><a href="${Previewlink}" class="social-icon" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
