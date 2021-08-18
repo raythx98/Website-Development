9f32f2fe7e3bf7a31b765fa1148fdb2f69c5e7bd
raythx98 <65889977+raythx98@users.noreply.github.com>
2021-08-18 17:44:00 +0800

    Add skill tab

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,7 @@ let header = $(
 +'  <ul class="navbar-nav ml-auto" id = "navbar-content">'
 +'   <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>'
 +'   <li class="nav-item"><a class="nav-link" href="experience.html">Experience</a></li>'
++'   <li class="nav-item"><a class="nav-link" href="skills.html">Skills</a></li>'
 +'   <li class="nav-item"><a class="nav-link" href="projects.html">Projects</a></li>'
 +'   <li class="nav-item"><a class="nav-link" href="research.html">Research</a></li>'
 +'   <li class="nav-item"><a class="nav-link" href="education.html">Education</a></li>'
